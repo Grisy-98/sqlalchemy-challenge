c204aef7c220c5417f8992f3e8ab72181d2e1740
Grisy-98 <rodriguezgriselda2054@gmail.com>
2024-10-24 22:59:41 -0500

    update code

--- Starter_Code/app.py
+++ Starter_Code/app.py
@@ -1,5 +1,14 @@
 # Import the dependencies.
+import numpy as np
+import pandas as pd
+import datetime as dt
+import re
 
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from flask import Flask, jsonify
 
 
 #################################################
@@ -7,26 +16,28 @@
 #################################################
 
 # Create engine using the `hawaii.sqlite` database file
-
+engine = create_engine("Starter_Code/Resources/hawaii.sqlite")
 # Declare a Base using `automap_base()`
-
+Base = automap_base()
 # Use the Base class to reflect the database tables
-
+Base.prepare(autoload_with=engine)
 
 # Assign the measurement class to a variable called `Measurement` and
 # the station class to a variable called `Station`
-
+Station = Base.classes.station
+Measurement = Base.classes.measurement
 
 # Create a session
-
+session = Session(engine)
 
 #################################################
 # Flask Setup
 #################################################
 
-
+app = Flask(__name__)
 
 
 #################################################
 # Flask Routes
 #################################################
+

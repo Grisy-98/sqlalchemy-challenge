d08acdc0a789a122e07a2f81f5f51f34f03b79a2
Grisy-98 <rodriguezgriselda2054@gmail.com>
2024-10-26 13:31:34 -0500

    update code

--- Starter_Code/app.py
+++ Starter_Code/app.py
@@ -17,7 +17,7 @@ from flask import Flask, jsonify
 #################################################
 
 # Create engine using the `hawaii.sqlite` database file
-engine = create_engine("sqlite:///hawaii.sqlite")
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 # Declare a Base using `automap_base()`
 Base = automap_base()
 # Use the Base class to reflect the database tables
